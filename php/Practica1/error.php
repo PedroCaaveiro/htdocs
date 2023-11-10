e4abdf75988574f82582510d74c446c6f4c8add6
PedroCaaveiro <pedrocaaveiro@estudiante.edib.es>
2023-11-10 20:42:26 +0100

    CAMBIOS PRACTICA PHP

--- php/Practica1/error.php
+++ php/Practica1/error.php
@@ -1,46 +1,54 @@
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style2.css">
 </head>
+
 <body>
     <div class="boton">
-    <input type="button" value="VOLVER" onclick="history.back()">
+        <input type="button" value="VOLVER" onclick="history.back()">
     </div>
-<?php
+    <?php
 
-$error=$_GET['cod_error'];
+    $error = $_GET['cod_error'];
 
-switch ($error) {
-    case '100':
-       $imagen='100.png';
-        break;
-        case '200':
-            $imagen='200.png';
+    switch ($error) {
+        case '100':
+            $imagen = '100.png';
             break;
-            case '300':
-                $imagen='300.png';
+        case '200':
+            $imagen = '200.png';
+            break;
+        case '300':
+            $imagen = '300.png';
+            break;
+        case '400':
+            $imagen = '400.png';
+            break;
+        case '500':
+            $imagen = '500.png';
+            break;
+            case '600':
+                $imagen = '600.png';
                 break;
-                case '400':
-                    $imagen='Error400.png';
+                case '700':
+                    $imagen = '700.png';
                     break;
-                    case '500':
-                        $imagen='error500.png';
+                    case '800':
+                        $imagen = '800.png';
                         break;
-                            
-    default:
-        
-        break;
-}
 
-echo '<img src="' . $imagen . '" alt="Descripción de la imagen">';
-?>
+        default:
+
+            break;
+    }
+
+    echo '<img src="' . $imagen . '" alt="Descripción de la imagen">';
+    ?>
 </body>
+
 </html>
